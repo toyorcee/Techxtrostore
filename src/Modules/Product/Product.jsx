934a6ae9255faa04e767acbacb48e97a1a7516f2
unknown <oluwatoyosiolaniyan@gmail.com>
2023-12-12 06:02:50 -0800

    responsiveness 12th Dec 2023

--- src/Modules/Product/Product.jsx
+++ src/Modules/Product/Product.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "./Product.css";
 
 const Product = () => {
   const { id } = useParams();
@@ -41,7 +42,7 @@ const Product = () => {
         JSON.stringify([...cart, { ...product, quantity: 1 }])
       );
     }
-    alert("Product added to cart")
+    alert("Product added to cart");
     if (redirect) {
       navigate("/cart");
     }
@@ -198,11 +199,25 @@ const Product = () => {
                 </div>
               </div>
             </div>
-            <div className="flex justify-between items-center">
-              <span className="title-font font-medium text-2xl text-gray-900">
-                ${product?.price}
-              </span>
-              <div className="flex justify-content-between">
+            <div className="flex justify-between items-center" id="fdc">
+              <div className="flex" id="top">
+                <span className="title-font font-medium text-2xl text-gray-900">
+                  ${product?.price}
+                </span>
+                <button className="rounded-full w-10 h-10 bg-blue-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              </div>
+              <div className="flex justify-content-between" id="btns">
                 <button
                   className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded-full mr-2"
                   onClick={() => handleCart(product, true)}
@@ -216,18 +231,6 @@ const Product = () => {
                   ADD TO CART
                 </button>
               </div>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
             </div>
           </div>
         </div>
