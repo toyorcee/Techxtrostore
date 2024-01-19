b86afff6b63dd0bd6e4d865af1cc98f06149d70d
unknown <oluwatoyosiolaniyan@gmail.com>
2024-01-18 20:39:36 -0800

    new css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import ecommerce from "../../Assets/ecomm.webp"
 
 const Hero = () => {
   return (
-    <section className="text-gray-600 body-font mt-20 bg-[#4CB9E7]">
+    <section className="text-gray-600 body-font mt-20">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
